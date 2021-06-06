4b38fafdb5f18a75b2bf50b3d8583609861255b0
Meet Godhani <meetgodhani23@gmail.com>
2021-06-06 11:59:36 -0400

    Clean state

--- src/bridge/mercury.js
+++
@@ -1,9 +0,0 @@
-import {
-  ipcRenderer
-} from 'electron'
-
-export default {
-  parseArticle: async (url) => {
-    return await ipcRenderer.invoke('parse-article', url)
-  }
-}
--- src/services/truncate.js
+++
@@ -1,5 +0,0 @@
-export default function (string, maxLength = 50) {
-  if (!string) return null
-  if (string.length <= maxLength) return string
-  return `${string.substring(0, maxLength)}...`
-}
